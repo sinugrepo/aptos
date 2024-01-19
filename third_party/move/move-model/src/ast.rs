f60b3f8a2cc5749e162c0dcfa6a910b4d8ae0a89
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2024-01-19 14:37:13 -0800

    add a lot of debugging for ast_simplifier

--- third_party/move/move-model/src/ast.rs
+++ third_party/move/move-model/src/ast.rs
@@ -2216,7 +2216,7 @@ impl ExpData {
             env: fun_env.module_env.env,
             exp: self,
             fun_env: Some(fun_env),
-            verbose: false,
+            verbose: true,
         }
     }
 
@@ -2252,7 +2252,7 @@ impl<'a> fmt::Display for ExpDisplay<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
         use ExpData::*;
         if self.verbose {
-            write!(f, "(")?;
+            write!(f, "({}: ", self.exp.node_id().as_usize())?;
         }
         match self.exp {
             Invalid(_) => write!(f, "*invalid*"),
